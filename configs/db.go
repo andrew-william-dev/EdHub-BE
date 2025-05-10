f1f4516525c8fde8f8043fccd88df79777ceb2c6
Andrew <andrewgithub010@gmail.com>
2025-05-10 11:27:43 +0530

    Updated env

--- configs/db.go
+++ configs/db.go
@@ -7,7 +7,7 @@ import (
 )
 
 func ConnectDB() *sql.DB {
-	connStr := os.Getenv("DATABASE_URL")
+	connStr := os.Getenv("CONN_STR")
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal("Failed to connect with the DataBase:", err)
