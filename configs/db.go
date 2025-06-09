2d89bf252dcc7986c3818bc2088219aed27e54dc
Andrew <andrewgithub010@gmail.com>
2025-06-10 00:05:35 +0530

    Removed logs

--- configs/db.go
+++ configs/db.go
@@ -7,16 +7,10 @@ import (
 )
 
 func ConnectDB() *sql.DB {
-	log.Println("Connecting to DB:", os.Getenv("CONN_STR"))
-	connStr := "postgresql://neondb_owner:npg_7YKfMJCqWe2l@ep-raspy-violet-a95zz6n1-pooler.gwc.azure.neon.tech/neondb?sslmode=require"
+	connStr := os.Getenv("CONN_STR")
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal("Failed to connect with the DataBase:", err)
-	}
-
-	err = db.Ping()
-	if err != nil {
-		log.Fatal("Failed to ping DB:", err)
 	}
 
 	return db
